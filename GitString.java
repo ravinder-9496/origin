8380411938e3f7bf4da9dc4c6617fce590821d58
ravinder-9496 <54262615+ravinder-9496@users.noreply.github.com>
2019-08-19 13:32:05 +0530

    Add files via upload

---
+++ GitString.java
@@ -0,0 +1,12 @@
+
+public class GitString {
+public GitString(String st)
+{
+	int l=st.length();
+	String up=st.toUpperCase();
+	String low=st.toLowerCase();
+	System.out.println("Length of "+st+" = "+l);
+	System.out.println("Upper Case:"+up);
+	System.out.println("Lower Case:"+low);
+}
+}
---
+++ gitClass.java
@@ -0,0 +1,10 @@
+public class gitClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is the Git Project");
+		
+		n.makeCalc(10, 5,"-");
+		GitString s = new GitString("git ProJect");
+	}
+
+}
